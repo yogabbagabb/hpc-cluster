71325ef695a71b3c4ebe697f30b45fe3a5070f8b
howilove <howilovetheearth@gmail.com>
2022-03-19 14:58:01 +0000

    Configuration for ec2 instance in each vpc Take 1

--- main.tf
+++ main.tf
@@ -9,3 +9,33 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
 }
 
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+
+data "aws_ami" "example" {
+  most_recent      = true
+
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+
+resource "aws_instance" "web" {
+  for_each  = toset(module.vpc.private_subnets)
+  subnet_id = each.value
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+}
+
